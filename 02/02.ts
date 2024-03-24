6d337cb3320f0ec251da97a2e1f4935b0c1d4f29
Evokoo <evanjohncooper@gmail.com>
2024-03-25 01:18:23 +1100

    Day 02 complete

--- 02/02.ts
+++ 02/02.ts
@@ -10,16 +10,19 @@ export function solveA(fileName: string, day: string): number {
 	return result;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		directions = parseInput(data),
+		result = navigateSubmarine(directions, true);
+
+	return result;
 }
 
 //Run
-solveA("example_a", "02");
+solveB("example_b", "02");
 
 // Functions
 type Direction = { direction: string; amount: number };
-type Submarine = { x: number; y: number };
+type Submarine = { x: number; depth: number; aim: number };
 
 function parseInput(data: string) {
 	const directions: Direction[] = [];
@@ -31,22 +34,41 @@ function parseInput(data: string) {
 
 	return directions;
 }
-function navigateSubmarine(directions: Direction[]) {
-	const submarine: Submarine = { x: 0, y: 0 };
+function navigateSubmarine(directions: Direction[], partB: boolean = false) {
+	const submarine: Submarine = { x: 0, depth: 0, aim: 0 };
 
 	for (const { direction, amount } of directions) {
-		switch (direction) {
-			case "f":
-				submarine.x += amount;
-				break;
-			case "d":
-				submarine.y += amount;
-				break;
-			case "u":
-				submarine.y -= amount;
-				break;
+		if (partB) {
+			switch (direction) {
+				case "f":
+					submarine.x += amount;
+					submarine.depth += submarine.aim * amount;
+					break;
+				case "d":
+					submarine.aim += amount;
+					break;
+				case "u":
+					submarine.aim -= amount;
+					break;
+				default:
+					throw Error("Invalid command");
+			}
+		} else {
+			switch (direction) {
+				case "f":
+					submarine.x += amount;
+					break;
+				case "d":
+					submarine.depth += amount;
+					break;
+				case "u":
+					submarine.depth -= amount;
+					break;
+				default:
+					throw Error("Invalid command");
+			}
 		}
 	}
 
-	return submarine.x * submarine.y;
+	return submarine.x * submarine.depth;
 }
