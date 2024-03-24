ea976e8ddede3a0fe124e523a425e8932f0d414b
Evokoo <evanjohncooper@gmail.com>
2024-03-25 01:08:06 +1100

    Day 02-1 complete

--- 02/02.ts
+++ 02/02.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		directions = parseInput(data),
+		result = navigateSubmarine(directions);
+
+	return result;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,35 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "02");
 
 // Functions
+type Direction = { direction: string; amount: number };
+type Submarine = { x: number; y: number };
+
+function parseInput(data: string) {
+	const directions: Direction[] = [];
+
+	for (let line of data.split("\r\n")) {
+		const [direction, amount] = line.split(" ");
+		directions.push({ direction: direction[0], amount: +amount });
+	}
+
+	return directions;
+}
+function navigateSubmarine(directions: Direction[]) {
+	const submarine: Submarine = { x: 0, y: 0 };
+
+	for (const { direction, amount } of directions) {
+		switch (direction) {
+			case "f":
+				submarine.x += amount;
+				break;
+			case "d":
+				submarine.y += amount;
+				break;
+			case "u":
+				submarine.y -= amount;
+				break;
+		}
+	}
+
+	return submarine.x * submarine.y;
+}
