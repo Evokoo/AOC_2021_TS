5e2b226a08922abb2b631a8f9fad5b24578df363
Evokoo <evanjohncooper@gmail.com>
2024-03-25 00:57:08 +1100

    Day 01 complete

--- 01/01.test.ts
+++ 01/01.test.ts
@@ -17,11 +17,11 @@ describe(`AOC 2021 - Day ${currentDay}`, () => {
 
 	describe("Part B", () => {
 		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
+			expect(solveB("example_b", currentDay)).toBe(5);
 		});
 
 		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
+			expect(solveB("input", currentDay)).toBe(1543);
 		});
 	});
 });
--- 01/01.ts
+++ 01/01.ts
@@ -10,27 +10,33 @@ export function solveA(fileName: string, day: string): number {
 	return count;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		scan = parseInput(data),
+		count = depthIncreases(scan, true);
+
+	return count;
 }
 
 //Run
-solveA("example_a", "01");
+solveB("example_b", "01");
 
 // Functions
 function parseInput(data: string) {
 	return data.split("\r\n").map(Number);
 }
-function depthIncreases(scan: number[]) {
-	let current = scan.shift()!;
+function depthIncreases(scan: number[], partB: boolean = false) {
+	let currentDepth = Infinity;
 	let increases = 0;
 
-	for (let depth of scan) {
-		if (depth > current) {
+	for (let i = 0; i < scan.length; i++) {
+		const [a, b, c] = [scan[i], scan[i + 1] ?? 0, scan[i + 2] ?? 0];
+		const depth = partB ? a + b + c : a;
+
+		if (depth > currentDepth) {
 			increases++;
 		}
 
-		current = depth;
+		currentDepth = depth;
 	}
 
 	return increases;
