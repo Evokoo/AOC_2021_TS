e4fa724c0dc1581e7194dccd1c1e499e04b23f05
Evokoo <evanjohncooper@gmail.com>
2024-03-25 00:46:16 +1100

    Day 01-1 complete

--- 01/01.ts
+++ 01/01.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		scan = parseInput(data),
+		count = depthIncreases(scan);
+
+	return count;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,20 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "01");
 
 // Functions
+function parseInput(data: string) {
+	return data.split("\r\n").map(Number);
+}
+function depthIncreases(scan: number[]) {
+	let current = scan.shift()!;
+	let increases = 0;
+
+	for (let depth of scan) {
+		if (depth > current) {
+			increases++;
+		}
+
+		current = depth;
+	}
+
+	return increases;
+}
