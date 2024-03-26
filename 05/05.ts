8fa3bcc0be2e0d37196b1d4343a22e67ba37da04
Evokoo <evanjohncooper@gmail.com>
2024-03-27 00:29:02 +1100

    Day 05-1 complete

--- 05/05.ts
+++ 05/05.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		points = parseInput(data),
+		intersections = findIntersections(points);
+
+	return intersections;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,35 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "05");
 
 // Functions
+type Points = Map<string, number>;
+
+function parseInput(data: string) {
+	const points: Points = new Map();
+
+	for (let line of data.split("\r\n")) {
+		const [x1, y1, x2, y2] = (line.match(/\d+/g) || []).map(Number);
+
+		if (x1 === x2 || y1 === y2) {
+			const [lx, hx] = [x1, x2].sort((a, b) => a - b);
+			const [ly, hy] = [y1, y2].sort((a, b) => a - b);
+
+			for (let x = lx; x <= hx; x++) {
+				for (let y = ly; y <= hy; y++) {
+					const point = `${x},${y}`;
+					points.set(point, (points.get(point) ?? 0) + 1);
+				}
+			}
+		}
+	}
+
+	return points;
+}
+function findIntersections(points: Points) {
+	let intersections = 0;
+
+	for (let [_, count] of points) {
+		if (count > 1) intersections++;
+	}
+
+	return intersections;
+}
