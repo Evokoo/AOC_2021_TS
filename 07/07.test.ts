d96937b3d1abb1b2960412cedf0603c56636278e
Evokoo <evanjohncooper@gmail.com>
2024-03-28 00:22:05 +1100

    Day 07 complete

--- 07/07.test.ts
+++ 07/07.test.ts
@@ -7,21 +7,21 @@ const currentDay = path.basename(__dirname);
 describe(`AOC 2021 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		test("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(null);
+			expect(solveA("example_a", currentDay)).toBe(37);
 		});
 
 		test("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(null);
+			expect(solveA("input", currentDay)).toBe(347449);
 		});
 	});
 
 	describe("Part B", () => {
 		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
+			expect(solveB("example_b", currentDay)).toBe(168);
 		});
 
 		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
+			expect(solveB("input", currentDay)).toBe(98039527);
 		});
 	});
 });
--- 07/07.ts
+++ 07/07.ts
@@ -10,25 +10,29 @@ export function solveA(fileName: string, day: string): number {
 	return fuel;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		crabs = parseInput(data),
+		fuel = alignCrabs(crabs, true);
+
+	return fuel;
 }
 
 //Run
-solveA("input", "07");
+solveB("example_b", "07");
 
 // Functions
 function parseInput(data: string) {
 	return data.split(",").map(Number);
 }
-function alignCrabs(crabs: number[]) {
+function alignCrabs(crabs: number[], partB: boolean = false) {
 	let minimumFuel = Infinity;
 
 	for (let i = 0; i < Math.max(...crabs); i++) {
 		let fuel = 0;
 
 		for (let crab of crabs) {
-			fuel += Math.abs(i - crab);
+			const steps = Math.abs(i - crab);
+			fuel += partB ? (steps / 2) * (1 + steps) : steps;
 		}
 
 		minimumFuel = Math.min(minimumFuel, fuel);
