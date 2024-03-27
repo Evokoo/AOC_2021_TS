1e2e141de8e2b58402245fea558d658dabcbeb5a
Evokoo <evanjohncooper@gmail.com>
2024-03-28 00:13:04 +1100

    Day 07-1 complete

--- 07/07.ts
+++ 07/07.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		crabs = parseInput(data),
+		fuel = alignCrabs(crabs);
+
+	return fuel;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -12,6 +15,24 @@ export function solveB(fileName: string, day: string): number {
 }
 
 //Run
-solveA("example_a", "07");
+solveA("input", "07");
 
 // Functions
+function parseInput(data: string) {
+	return data.split(",").map(Number);
+}
+function alignCrabs(crabs: number[]) {
+	let minimumFuel = Infinity;
+
+	for (let i = 0; i < Math.max(...crabs); i++) {
+		let fuel = 0;
+
+		for (let crab of crabs) {
+			fuel += Math.abs(i - crab);
+		}
+
+		minimumFuel = Math.min(minimumFuel, fuel);
+	}
+
+	return minimumFuel;
+}
