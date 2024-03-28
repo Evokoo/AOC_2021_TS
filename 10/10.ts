a985269cb55f8843e17d896eb56789b50877e9d1
Evokoo <evanjohncooper@gmail.com>
2024-03-29 01:08:58 +1100

    Day 10 complete

--- 10/10.ts
+++ 10/10.ts
@@ -10,12 +10,16 @@ export function solveA(fileName: string, day: string): number {
 	return score;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		lines = parseInput(data),
+		{ validLines } = validateLines(lines),
+		score = completeLines(validLines);
+
+	return score;
 }
 
 //Run
-solveA("example_a", "10");
+solveB("example_b", "10");
 
 // Functions
 function parseInput(data: string) {
@@ -30,7 +34,7 @@ function validateLines(lines: string[]) {
 	};
 
 	let score: number = 0;
-	let valid: string[] = [];
+	let validLines: string[] = [];
 
 	for (let line of lines) {
 		while (/\(\)|\[\]|\{\}|<>/.test(line)) {
@@ -39,9 +43,41 @@ function validateLines(lines: string[]) {
 
 		const mismatch = (line.match(/[\[\(\{\<][\)\}\]\>]/) || [])[0];
 
-		if (!mismatch) valid.push(line);
+		if (!mismatch) validLines.push(line);
 		else score += value[mismatch[1]];
 	}
 
-	return { valid, score };
+	return { validLines, score };
+}
+function completeLines(lines: string[]) {
+	const scores: number[] = [];
+
+	for (let line of lines) {
+		let lineScore = 0;
+
+		for (let i = line.length - 1; i >= 0; i--) {
+			lineScore *= 5;
+
+			switch (line[i]) {
+				case "(":
+					lineScore += 1;
+					break;
+				case "[":
+					lineScore += 2;
+					break;
+				case "{":
+					lineScore += 3;
+					break;
+				case "<":
+					lineScore += 4;
+					break;
+				default:
+					throw Error("Invalid character");
+			}
+		}
+
+		scores.push(lineScore);
+	}
+
+	return scores.sort((a, b) => a - b)[~~(scores.length / 2)];
 }
