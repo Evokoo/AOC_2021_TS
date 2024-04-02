3c5cd9da704296413a529d80af54a90f0bc1bb10
Evokoo <evanjohncooper@gmail.com>
2024-04-03 03:09:03 +1100

    Day 17-1 complete

--- 17/17.test.ts
+++ 17/17.test.ts
@@ -7,7 +7,7 @@ const currentDay = path.basename(__dirname);
 describe(`AOC 2021 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		test("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(null);
+			expect(solveA("example_a", currentDay)).toBe(45);
 		});
 
 		test("Solution", () => {
@@ -15,13 +15,13 @@ describe(`AOC 2021 - Day ${currentDay}`, () => {
 		});
 	});
 
-	describe("Part B", () => {
-		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
-		});
+	// describe("Part B", () => {
+	// 	test("Example", () => {
+	// 		expect(solveB("example_b", currentDay)).toBe(null);
+	// 	});
 
-		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
-		});
-	});
+	// 	test("Solution", () => {
+	// 		expect(solveB("input", currentDay)).toBe(null);
+	// 	});
+	// });
 });
--- 17/17.ts
+++ 17/17.ts
@@ -3,8 +3,11 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		range = parseInput(data),
+		trajectory = getTrajectory(range);
+
+	return trajectory.maxY;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -15,3 +18,63 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "17");
 
 // Functions
+type Point = { x: number; y: number };
+type Trajectory = Point & { maxY: number };
+type Range = [number, number, number, number];
+
+function parseInput(data: string): Range {
+	return (data.match(/-*\d+/g) || []).map(Number) as Range;
+}
+function inRange(point: Point, [x1, x2, y1, y2]: Range, axis?: string) {
+	if (axis === "Y") {
+		return point.y >= y1 && point.y <= y2;
+	}
+	if (axis === "X") {
+		return point.x >= x1 && point.x <= x2;
+	}
+
+	return point.x >= x1 && point.x <= x2 && point.y >= y1 && point.y <= y2;
+}
+function onTarget(
+	probe: Point,
+	velocity: Point,
+	[x1, x2, y1, y2]: Range
+): [boolean, number] {
+	let maxY = probe.y;
+
+	while (probe.x < x2 && probe.y > y2) {
+		probe.x += velocity.x;
+		probe.y += velocity.y;
+
+		maxY = Math.max(probe.y, maxY);
+
+		velocity.y--;
+		if (velocity.x !== 0) {
+			velocity.x > 0 ? velocity.x-- : velocity.x++;
+		}
+
+		if (inRange(probe, [x1, x2, y1, y2])) {
+			return [true, maxY];
+		}
+	}
+
+	return [false, 0];
+}
+
+function getTrajectory(range: Range) {
+	const trajectory: Trajectory = { x: 0, y: 0, maxY: 0 };
+
+	for (let x = 0; x < 1000; x++) {
+		for (let y = 0; y < 1000; y++) {
+			const [success, maxY] = onTarget({ x: 0, y: 0 }, { x, y }, range);
+
+			if (success && maxY > trajectory.maxY) {
+				trajectory.x = x;
+				trajectory.y = y;
+				trajectory.maxY = maxY;
+			}
+		}
+	}
+
+	return trajectory;
+}
